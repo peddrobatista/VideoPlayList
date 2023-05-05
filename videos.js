e7bff676fb7ecb345ac62be75b4bea59ff2407ab
pedro-batista <peddrobatista2022@gmail.com>
2023-05-05 15:10:38 -0300

    Primeiro commit...

---
+++ videos.js
@@ -0,0 +1,32 @@
+const videos = [
+    {
+        src: "./midia/arranha_ceu.mp4",
+        title: "Arranha Céu"
+    },
+    {
+        src: "./midia/aurora_borealis.mp4",
+        title: "Aurora Boreal"
+    },
+    {
+        src: "./midia/montanhas.mp4",
+        title: "Montanhas"
+    },
+    {
+        src: "./midia/mulher.mp4",
+        title: "Mulher"
+    },
+    {
+        src: "./midia/natal.mp4",
+        title: "Natal"
+    },
+    {
+        src: "./midia/paraquedas.mp4",
+        title: "Paraquedas"
+    },
+    {
+        src: "./midia/relva.mp4",
+        title: "Relva"
+    }
+];
+
+export default videos;
